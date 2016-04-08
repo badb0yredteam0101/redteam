c94ad8bdfd026c8ca616a73af9be2a86a853f284
p00ria <amir1300sobatdar@gmail.com>
2016-04-08 21:11:51 +0430

    Update and rename owners.lua to Owners.lua

--- plugins/Owners.lua
+++ plugins/Owners.lua
@@ -306,12 +306,12 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]owners (%d+) ([^%s]+) (.*)$",
-    "^[!/]owners (%d+) ([^%s]+)$",
-    "^[!/](changeabout) (%d+) (.*)$",
-    "^[!/](changerules) (%d+) (.*)$",
-    "^[!/](changename) (%d+) (.*)$",
-		"^[!/](loggroup) (%d+)$"
+    "^[!/#]owners (%d+) ([^%s]+) (.*)$",
+    "^[!/#]owners (%d+) ([^%s]+)$",
+    "^[!/#](changeabout) (%d+) (.*)$",
+    "^[!/#](changerules) (%d+) (.*)$",
+    "^[!/#](changename) (%d+) (.*)$",
+		"^[!/#](loggroup) (%d+)$"
   },
   run = run
 }
