ed4ca47ffbab55b17b180691d15049e4e1fa03cd
p00ria <amir1300sobatdar@gmail.com>
2016-04-15 21:30:48 +0430

    Update and rename Sudoers.lua to Add_Sudo.lua

---
+++ plugins/Add_Sudo.lua
@@ -0,0 +1,28 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+local function run(msg, matches)
+      local sudo = 175636120 --Put you id Here !
+    local addsudo = 'user#id'..sudo
+  local chat = get_receiver(msg)
+  if is_momod(msg) then  -- you can set it to is_owner(msg) 
+    chat_add_user(chat, addsudo, callback, false)
+    end
+end
+
+return {
+  patterns = {
+    "^[#!/][Aa][Dd][Dd][Ss][uU][Dd][oO]$",
+    "^[Aa][Dd][Dd][Ss][uU][Dd][oO]$",
+    "^[#!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+    "^[Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+  }, 
+  run = run
+  
+}
+
+end
+
