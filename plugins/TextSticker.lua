3cd0f2ad5e7ef44a8caa382c6bdfa483c5301dec
p00ria <amir1300sobatdar@gmail.com>
2016-05-10 20:29:59 +0430

    Update TextSticker.lua

--- plugins/TextSticker.lua
+++ plugins/TextSticker.lua
@@ -1,33 +1,63 @@
-do
-
-local function send_title(cb_extra, success, result)
-  if success then
-  send_msg(cb_extra[1], cb_extra[2], ok_cb, false)
- end
-end
-
 local function run(msg, matches)
-    
-  local eq = URL.escape(matches[2])
-local size = URL.escape(matches[1])
-  local url = "http://latex.codecogs.com/png.download?"
-    .."\\dpi{"..size.."}%80\\LARGE%80"..eq
-
-  local receiver = get_receiver(msg)
-  local title = "Edit LaTeX on www.codecogs.com/eqnedit.php?latex="..eq
-  local file = download_to_file(url,'hextor.webp')
-      send_document('chat#id'..msg.to.id, file, ok_cb , false)
+        local text = URL.escape(matches[1])
+        local color = 'blue'
+        if matches[2] == 'red' then
+            color = 'red'
+        elseif matches[2] == 'black' then
+            color = 'black'
+      elseif matches[2] == 'blue' then
+          color = 'blue'
+    elseif matches[2] == 'green' then
+        color = 'green'
+    elseif matches[2] == 'yellow' then
+        color = 'yellow'
+    elseif matches[2] == 'pink' then
+        color = 'magenta'
+    elseif matches[2] == 'orange' then
+        color = 'Orange'
+    elseif matches[2] == 'brown' then
+        color = 'DarkOrange'
+        end
+        local font = 'mathrm'
+        if matches[3] == 'bold' then
+            font = 'mathbf'
+        elseif matches[3] == 'italic' then
+            font = 'mathit'
+        elseif matches[3] == 'fun' then
+            font = 'mathfrak'
+        elseif matches[1] == 'arial' then
+            font = 'mathrm'
+        end
+        local size = '700'
+        if matches[4] == 'small' then 
+            size = '300'
+        elseif matches[4] == 'larg' then
+            size = '700'
+            end
+local url = 'http://latex.codecogs.com/png.latex?'..'\\dpi{'..size..'}%20\\huge%20\\'..font..'{{\\color{'..color..'}'..text..'}}'
+local file = download_to_file(url,'file.webp')
+if msg.to.type == 'chat' then
+send_document('chat#id'..msg.to.id,file,ok_cb,false)
 end
-
+end
 return {
-  description = "Convert LaTeX equation to sticker",
-  usage = {
-    "/sticker [equation]: Convert LaTeX equation to sticker"
-  },
-  patterns = {
-    "^[!/#]sticker (.+) (.+)$"
-  },
-  run = run
+   patterns = {
+       "^[/!#]sticker (.*) ([^%s]+) (.*) (small)$",
+       "^[/!#]sticker (.*) ([^%s]+) (.*) (larg)$",
+       "^[/!#]sticker (.*) ([^%s]+) (bold)$",
+ "^[/!#]sticker (.*) (bold)$",
+       "^[/!#]sticker (.*) ([^%s]+) (italic)$",
+       "^[/!#]sticker (.*) ([^%s]+) (fun)$",
+       "^[/!#]sticker (.*) ([^%s]+) (arial)$",
+       "^[/!#]sticker (.*) (red)$",
+       "[/!#]sticker (.*) (black)$",
+       "^[/!#]sticker (.*) (blue)$",
+       "^[/!#]sticker (.*) (green)$",
+       "^[/!#]sticker (.*) (yellow)$",
+       "^[/!#]sticker (.*) (pink)$",
+       "^[/!#]sticker (.*) (orange)$",
+       "^[/!#]sticker (.*) (brown)$",
+       "^[/!#]sticker +(.*)$",
+       },
+   run = run
 }
-
-end
